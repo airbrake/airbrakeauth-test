815a97cf821436b8e1376c3fb05d4a63e83b88cf
Muhammad Umar <21365827+umarmuha@users.noreply.github.com>
2022-03-28 13:35:38 -0500

    Adds correct subdomain property in the post req
    Updated the Axios post request to include the subdomain property

--- src/Unifiedapi.js
+++ src/Unifiedapi.js
@@ -2,39 +2,40 @@ import React from 'react';
 import axios from 'axios';
 
 export default function Unifiedapi() {
-  const cookieHeader = 'gAAAAABiMmTidIyRVze1zCX4DCFs9UiNeZD0ynU3y8IAkiaS1jurfqZherkKKgzqtspAOfzYo_gfyD_hBCPHSzWb3BQMl25dVAJ_Mrs3267Z2Et9Te_kEC0=';
-  const key = '8ce285fee9139b59c7dab09e6947b1a4e7aa70d6';
+  //   const cookieHeader = 'gAAAAABiMmTidIyRVze1zCX4DCFs9UiNeZD0ynU3y8IAkiaS1jurfqZherkKKgzqtspAOfzYo_gfyD_hBCPHSzWb3BQMl25dVAJ_Mrs3267Z2Et9Te_kEC0=';
+  //   const key = '8ce285fee9139b59c7dab09e6947b1a4e7aa70d6';
+
+  //   const formData = new FormData();
+  //   formData.append('email', 'login@test.com');
+  //   formData.append('password', 'logintest1');
 
   const headers = {
     'Content-Type': 'application/json',
   };
 
   // Getting project list
-  axios
-    .get(`https://uapi.err.io/2021-07-04/projects?key=${cookieHeader}`, { headers })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  //   axios
+  //     .get(`https://uapi.err.io/2021-07-04/projects?key=${cookieHeader}`, { headers })
+  //     .then((response) => {
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
 
   // Sending login info to get reponse token.
   axios
     .post(
-      // 'http://localhost:7003/2021-07-04/sessions',
       'https://uapi.err.io/2021-07-04/sessions',
-      //   `https://err.io/api/v4/projects?key=${key}`,
-      //   `https://uapi.err.io/2021-07-04/projects?key=${key}`,
       {
-        email: 'logintest@airbrake.io',
-        password: 'Login1234',
+        subdomain: 'login-test-com',
+        email: 'login@test.com',
+        password: 'logintest1',
       },
-      { withCredentials: true },
       { headers }
     )
     .then((response) => {
-      console.log(response.data);
+      console.log(response.data.token);
     })
     .catch((error) => {
       console.log(error);
